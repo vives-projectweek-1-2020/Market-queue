464f912a9f0f6591f3e3f0f26f077eb6cc9b8790
Dimi Catrysse <dimicatrysse@outlook.com>
2020-04-20 19:08:09 +0200

    Made c# program to insert the coordinates from 'speelruimte.json' into the table market_queue.area

---
+++ database/InsertValues/InsertValues/Database.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace InsertValues
+{
+    // Based on https://www.codeproject.com/Articles/43438/Connect-C-to-MySQL
+    class Database
+    {
+        private MySqlConnection connection;
+        private string server = "localhost";
+        private string uid = "root";
+        private string password = "";
+
+        //Constructor
+        public Database()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            string connectionString;
+            connectionString = "SERVER=" + server + ";"+ "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+        }
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException)
+            {
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException)
+            {
+                return false;
+            }
+        }
+        
+        public bool ExecuteNonQuery(string query)
+        {
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ database/InsertValues/InsertValues/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Serialization;
+
+namespace InsertValues
+{
+    class Program
+    {
+        private static Database database;
+        private static string[] queries =
+        {
+            "DROP DATABASE IF EXISTS market_queue;",
+            "CREATE DATABASE market_queue;",
+            "USE market_queue;",
+            "CREATE TABLE area(id int(5) NOT NULL AUTO_INCREMENT,latitude DECIMAL(15,13) NOT NULL,longitude DECIMAL(15,13) NOT NULL,visitors SMALLINT UNSIGNED DEFAULT 0,PRIMARY KEY(id));"
+        };
+        static void Main(string[] args)
+        {
+            database = new Database();
+            foreach(string query in queries)
+            {
+                ExecuteNonQuery(query);
+            }
+            string content = File.ReadAllText("./speelruimte.json");
+            var data = JsonConvert.DeserializeObject<JArray>(content);
+            foreach (JToken child in data.Children())
+            {
+                string longitude = (child["json_geometry"]["coordinates"][0]).ToString().Replace(',','.');
+                string latitude = (child["json_geometry"]["coordinates"][1]).ToString().Replace(',', '.');
+                ExecuteNonQuery($"INSERT INTO area (latitude,longitude) VALUES ({latitude},{longitude});");
+            }
+            Console.WriteLine("Database is set!");
+        }
+        static void ExecuteNonQuery(string query)
+        {
+            bool result = database.ExecuteNonQuery(query);
+            if (!result)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+        }
+    }
+}
