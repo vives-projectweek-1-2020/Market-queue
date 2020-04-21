ec4901efaa68d29a7b5083d3c04dfbb1ab04d905
Aaron Van Vyve <aaron.van.vyve@telenet.be>
2020-04-21 18:53:28 +0200

    GoingOut layout plus funtions plus confirmation

--- Site/JavaScript/GoingOut.js
+++ Site/JavaScript/GoingOut.js
@@ -1,35 +1,43 @@
+var inputPlace=" ";
+var inputTime=0;
+var inputArriveTime =0;
+
+
 function GetPlace(){
-    var inputPlace = document.getElementById("visitPlace").value
+     inputPlace = document.getElementById("visitPlace").value
             
-     
-    console.log("visit place= " + inputPlace)
 }
 
 function GetArriveTime(){
-    var inputArriveTime = document.getElementById("arriveTime").value
-            
-     
-    console.log("arrive time= " + inputArriveTime +"mins")
+    inputArriveTime = document.getElementById("arriveTime").value
 
 }
 
 function GetVisitTime(){
       
-    var inputTime = document.getElementById("visitTime").value
-            
-     
-    console.log("visit time= " + inputTime +"mins")
-    
-    
+    inputTime = document.getElementById("visitTime").value
+ 
 }
 
 
 
+function ToConsole(){
+    console.log("visit place= " + inputPlace)
+    console.log("arrive time= " + inputArriveTime +" mins")
+    console.log("visit time= " + inputTime +" mins")
+}
+
 
 
 document.getElementById('EnterButton').onclick = () => {
+    
     GetPlace();
     GetArriveTime();
     GetVisitTime();
+    if(
+        window.confirm("Please confirm:\n" +"Place: " +inputPlace + ", Arrive time: "+inputArriveTime+" min"+ ", Visit time: "+inputTime +" min")
+    ){
+    ToConsole();
+    }
     
 }
