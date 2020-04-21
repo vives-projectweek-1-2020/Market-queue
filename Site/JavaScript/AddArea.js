2ad827b507d91503663ca392bbfa1b37670d63a3
Felix Falk <felix.falk2903@gmail.com>
2020-04-21 15:27:04 +0200

    added the adding page, now only need to push the result to the database

--- Site/JavaScript/AddArea.js
+++ Site/JavaScript/AddArea.js
@@ -3,7 +3,20 @@ let x = document.getElementById("text");
 let latitude;
 let Longitude;
 let jsonData;
-
+let streetNamePlaceInput;
+let villageNameInput;
+document.getElementById("streetNamePlace").placeholder = "Street name here";
+document.getElementById("villageName").placeholder = "Village/city name here";
+function geolocationOrAdress(){
+  streetNamePlaceInput = document.getElementById("streetNamePlace").value;
+  villageNameInput = document.getElementById("villageName").value;
+  if (document.getElementById("isAtPlace").checked){
+    getLocation();
+  }
+  else{
+    getCoordinates();
+  }
+}
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -15,9 +28,8 @@ function getLocation() {
 function showPosition(position) {
 latitude = position.coords.latitude;
 longitude = position.coords.longitude;
- // x.innerHTML = "Latitude: " + position.coords.latitude + 
-  //"<br>Longitude: " + position.coords.longitude;
-getPlace().then(data=> console.log(data));
+  x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+  getPlace().then(data=> console.log(data));
 }
 
 
@@ -29,3 +41,10 @@ async function getPlace(){
     return data;
 }
 
+async function getCoordinates(){
+  let Response = await fetch('https://api.opencagedata.com/geocode/v1/json?q=' + streetNamePlaceInput +  "%20" + villageNameInput + '&key=6e978319e06444d481d5ac3f328be3ef');
+  let data = await Response.json()
+  jsonData = data;
+  x.innerHTML = "Latitude: " + jsonData.results[0].geometry.lat + "<br>Longitude: " + jsonData.results[0].geometry.lng;
+  console.log(jsonData.results[0].geometry.lat + "+" + jsonData.results[0].geometry.lng);
+}
