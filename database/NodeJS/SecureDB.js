dff499068d6365ba7a23352e7b98700c48c6304d
Dimi Catrysse <dimicatrysse@outlook.com>
2020-04-22 14:27:50 +0200

    Made REST API for getting date from the visitor table

--- database/NodeJS/SecureDB.js
+++ database/NodeJS/SecureDB.js
@@ -16,12 +16,11 @@ connection.connect((error)=>{
   else { console.log("Connected to DB"); }
 })
 
-//AREA
+//GET AREA
 app.get('/get/area', function(req, res) {
   if(JSON.stringify(req.query) == "{}")
   {
     let query = 'SELECT * FROM market_queue.area;';
-    console.log(query);
     connection.query(query, (error, rows)=>{
         if(!error){ res.end(JSON.stringify(rows)); }
         else { res.end("ERROR: " + JSON.stringify(error)); }
@@ -55,27 +54,88 @@ app.get('/get/area', function(req, res) {
   {
     res.end("=> GET NEAREST");
   }
+  else
+  {
+    res.end("ERROR: wrong input!");
+  }
 });
-app.get('/get', function(req, res) {
-  res.end(JSON.stringify(req.query));
-});
-app.get('/add/area/:columns/:values', function(req, res) {
-  let query = 'INSERT INTO market_queue.area (' + req.params.columns + ') VALUES (' + req.params.values + ');';
-  console.log(query);
-  connection.query(query, (error, rows)=>{
-      if(!error){ res.end("SUCCESS: " + JSON.stringify(rows)); }
-      else { res.end("ERROR: " + JSON.stringify(error)); }
-    });
+//ADD AREA
+app.get('/add/area', function(req, res) {
+  if(JSON.stringify(req.query) == "{}")
+  {
+    res.end("ERROR: no data given!");
+  }
+  else if(req.query.latitude != undefined && req.query.longitude != undefined && Object.keys(req.query).length == 2)
+  {
+    let query = 'INSERT INTO market_queue.area (latitude,longitude) VALUES (' + req.query.latitude + ',' + req.query.longitude + ');';
+    connection.query(query, (error, rows)=>{
+        if(!error){ res.end("SUCCESS: " + JSON.stringify(rows)); }
+        else { res.end("ERROR: " + JSON.stringify(error)); }
+      });
+  }
+  else
+  {
+    console.log(req.query);
+    res.end("ERROR: wrong input!");
+  }
 });
 
 //VISITOR
 app.get('/get/visitor', function(req, res) {
-  let query = 'SELECT * FROM market_queue.visitor;';
-  console.log(query);
-  connection.query(query, (error, rows)=>{
+  if(JSON.stringify(req.query) == "{}")
+  {
+    let query = 'SELECT * FROM market_queue.visitor where DATE_ADD(check_in_time,INTERVAL duration MINUTE)>NOW();';
+    connection.query(query, (error, rows)=>{
       if(!error){ res.end(JSON.stringify(rows)); }
       else { res.end("ERROR: " + JSON.stringify(error)); }
     });
+  }
+  else if (req.query.all == "true" && Object.keys(req.query).length == 1)
+  {
+    let query = 'SELECT * FROM market_queue.visitor;';
+    connection.query(query, (error, rows)=>{
+      if(!error){ res.end(JSON.stringify(rows)); }
+      else { res.end("ERROR: " + JSON.stringify(error)); }
+    });
+  }
+  else if (req.query.id != undefined && Object.keys(req.query).length == 1)
+  {
+    let query = 'SELECT * FROM market_queue.visitor where id =' + req.query.id + " AND DATE_ADD(check_in_time,INTERVAL duration MINUTE)>NOW();";
+    connection.query(query, (error, rows)=>{
+      if(!error){ res.end(JSON.stringify(rows)); }
+      else { res.end("ERROR: " + JSON.stringify(error)); }
+    });
+  }
+  else if (req.query.id != undefined && req.query.all == true && Object.keys(req.query).length == 2)
+  {
+    let query = 'SELECT * FROM market_queue.visitor where id =' + req.query.id + ";";
+    connection.query(query, (error, rows)=>{
+      if(!error){ res.end(JSON.stringify(rows)); }
+      else { res.end("ERROR: " + JSON.stringify(error)); }
+    });
+  }
+  else if (req.query.area_id != undefined && Object.keys(req.query).length == 1)
+  {
+    let query = 'SELECT * FROM market_queue.visitor where area_id =' + req.query.area_id + " AND DATE_ADD(check_in_time,INTERVAL duration MINUTE)>NOW();";
+    connection.query(query, (error, rows)=>{
+      if(!error){ res.end(JSON.stringify(rows)); }
+      else { res.end("ERROR: " + JSON.stringify(error)); }
+    });
+  }
+  else if (req.query.area_id != undefined && req.query.all == true && Object.keys(req.query).length == 2)
+  {
+    let query = 'SELECT * FROM market_queue.visitor where area_id =' + req.query.area_id + ";";
+    connection.query(query, (error, rows)=>{
+      if(!error){ res.end(JSON.stringify(rows)); }
+      else { res.end("ERROR: " + JSON.stringify(error)); }
+    });
+  }
+  else
+  {
+    console.log(req.query);
+    res.end("ERROR: wrong input!");
+  }
+  
 });
 app.get('/add/visitor/:columns/:values', function(req, res) {
   let query = 'INSERT INTO market_queue.visitor (' + req.params.columns + ') VALUES (' + req.params.values + ');';
@@ -85,3 +145,14 @@ app.get('/add/visitor/:columns/:values', function(req, res) {
       else { res.end("ERROR: " + JSON.stringify(error)); }
     });
 });
+/*
+else if(req.query.area_id != undefined && req.query.duration != undefined && req.query.offset != undefined && Object.keys(req.query).length == 3)
+  {
+    let query = 'INSERT INTO market_queue.area (area_id,check_in_time,duration) VALUES (' 
+              + req.query.area_id + ',ADDTIME(current_time(), ' + req.query.offset + '),' + req.query.duration + ');';
+    connection.query(query, (error, rows)=>{
+        if(!error){ res.end("SUCCESS: " + JSON.stringify(rows)); }
+        else { res.end("ERROR: " + JSON.stringify(error)); }
+      });
+  }
+*/
