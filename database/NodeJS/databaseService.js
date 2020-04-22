aa17ed851e6b04b47e1caf81830d65e0386f2017
Dimi Catrysse <dimicatrysse@outlook.com>
2020-04-22 08:44:16 +0200

    Fixed issue #1

--- database/NodeJS/databaseService.js
+++ database/NodeJS/databaseService.js
@@ -3,54 +3,54 @@ const app = express();
 const port = 3000;
 
 app.get('/:databasequery/:columns', function (req, res) {
+  console.log(req.params);
   var decodedDBquery = decodeURI(req.params["databasequery"]);
   var decodedColumns = decodeURI(req.params["columns"]);
   var columns = GetColumns(decodedColumns);
   var textToSend = "";
   var query = "" + GetQuery(decodedDBquery);
   console.log("Request: '" + query + "' with columns '" + columns + "'");
-  RunQueryWithResult(query,columns,function(results){
-    connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: ""
+  if(query != "")
+  {
+    RunQueryWithResult(query,columns,function(results){
+      SetConnection();
+      (results).forEach(result => {
+        textToSend += result + `<br>`
+      });
+    res.send(textToSend);
     });
-    (results).forEach(result => {
-      textToSend += result + `<br>`
-    });
-  res.send(textToSend);
-  });
+  }
 })
 app.get('/:databasequery', function (req, res) {
+  console.log(req.params);
   var decodedDBquery = decodeURI(req.params["databasequery"]);
   var textToSend = "";
   var query = "" + GetQuery(decodedDBquery);
   console.log("Request: '" + query + "'");
-  RunQuery(query,function(results){
-    connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: ""
+  if(query != "")
+  {
+    RunQuery(query,function(results){
+      SetConnection();
+      (results).forEach(result => {
+        textToSend += result + `<br>`
+      });
+    res.send(textToSend);
     });
-    (results).forEach(result => {
-      textToSend += result + `<br>`
-    });
-  res.send(textToSend);
-  });
-  
-  
+  }
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
-
+var connection;
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: ""
-});
-
+SetConnection();
+function SetConnection(){
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+  });
+}
 function GetColumns(input)
 {
   return input.split(',');
@@ -61,6 +61,10 @@ function GetQuery(url){
   {
     var query = url.substr(9);
     return query;
+  }
+  else
+  {
+    return "";
   }
 }
 
@@ -90,7 +94,6 @@ function RunQueryWithResult(query,columns,callback)
             rowData += row[column] + `\t,\t`;
           });
           returningResult.push(rowData.substr(0,rowData.length-2));
-        
       });
       return callback(returningResult);
     });
