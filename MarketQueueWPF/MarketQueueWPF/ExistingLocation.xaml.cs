6e444ba1650b0950e60bb9c07a1523c5285fa7d5
Felix Falk <felix.falk2903@gmail.com>
2020-04-22 11:41:46 +0200

    added some things in the places tab on wpf code behind

--- MarketQueueWPF/MarketQueueWPF/ExistingLocation.xaml.cs
+++ MarketQueueWPF/MarketQueueWPF/ExistingLocation.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
 
 namespace MarketQueueWPF
 {
@@ -23,9 +26,31 @@ namespace MarketQueueWPF
         public ExistingLocation()
         {
             InitializeComponent();
+            CalculateCoordinates();
         }
+        double place0Lng = 3.0912;
+        double place0Lat = 51.0586;
 
-       
+        private async void CalculateCoordinates()
+        {
+            try
+            {
+                string url = "https://api.opencagedata.com/geocode/v1/json?q=" + place0Lat + "+" + place0Lng + "&key=6e978319e06444d481d5ac3f328be3ef";
+                HttpClient client = new HttpClient();
+                HttpResponseMessage res = await client.GetAsync(url);
+                HttpContent content = res.Content;
+                var data = await content.ReadAsStringAsync();
+                var boe = JObject.Parse(data);
+                string place = boe["results"][0]["formatted"].ToString();
+                Console.WriteLine("\n latitude: " +place+ "\n");
 
+                LocationPlace0.Text = place;
+            }
+            catch
+            {
+                MessageBox.Show("there has been an error parsing your data");
+            }
+
+        }
     }
 }
